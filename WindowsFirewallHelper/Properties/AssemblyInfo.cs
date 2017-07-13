85e15c77fa4ae96e97ec326462889113f6900d29
s_falahati <s_falahati@hotmail.com>
2017-07-13 07:11:42 +0430

    NuGet scripts and build targets updated

--- WindowsFirewallHelper/Properties/AssemblyInfo.cs
+++ WindowsFirewallHelper/Properties/AssemblyInfo.cs
@@ -5,14 +5,14 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("Windows Firewall Helper")]
+[assembly: AssemblyTitle("WindowsFirewallHelper")]
 [assembly:
     AssemblyDescription(
-        "A .Net library to add or remove your program to the windows firewall exception list as well as managing the permitted protocols and ports"
+        "A class library to manage the Windows Firewall as well as adding your program to the Windows Firewall Exception list. Supporting Windows XP+."
         )]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Soroush Falahati")]
-[assembly: AssemblyProduct("Windows Firewall Helper")]
+[assembly: AssemblyProduct("WindowsFirewallHelper")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -38,5 +38,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]

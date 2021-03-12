8a3dfecc8865ab1917f06ffbbddbd8d997378fe8
Paul Irwin <paulirwin@gmail.com>
2021-03-12 09:52:42 -0700

    Add TryGetInstance and TryGetRegisteredProducts methods. Update test project dependencies.

---
+++ WindowsFirewallHelper.Tests/FirewallManagerTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+
+namespace WindowsFirewallHelper.Tests
+{
+    public class FirewallManagerTests
+    {
+        [Test]
+        public void TryGetInstanceTest()
+        {
+            // NOTE: this may vary depending on the machine on which it is run.
+            var successful = FirewallManager.TryGetInstance(out var instance);
+
+            if (successful)
+            {
+                Assert.IsNotNull(instance);
+            }
+            else
+            {
+                Assert.IsNull(instance);
+            }
+        }
+
+        [Test]
+        public void TryGetRegisteredProductsTest()
+        {
+            // NOTE: this may vary depending on the machine on which it is run.
+            var successful = FirewallManager.TryGetRegisteredProducts(out var collection);
+
+            if (successful)
+            {
+                Assert.IsNotNull(collection);
+            }
+            else
+            {
+                Assert.IsNull(collection);
+            }
+        }
+    }
+}

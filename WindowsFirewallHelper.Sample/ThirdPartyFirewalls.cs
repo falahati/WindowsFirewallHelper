f35858d3ef0d9c6acc803acf2b54e6412e4008e3
Greg Sohl <greg.sohl@sapiens.com>
2022-04-21 16:57:52 -0500

    Converted tabs to spaces

--- WindowsFirewallHelper.Sample/ThirdPartyFirewalls.cs
+++ WindowsFirewallHelper.Sample/ThirdPartyFirewalls.cs
@@ -3,19 +3,19 @@ using System.Windows.Forms;
 
 namespace WindowsFirewallHelper.Sample
 {
-	public partial class ThirdPartyFirewalls : Form
-	{
-		public ThirdPartyFirewalls()
-		{
-			InitializeComponent();
-		}
+    public partial class ThirdPartyFirewalls : Form
+    {
+        public ThirdPartyFirewalls()
+        {
+            InitializeComponent();
+        }
 
-		private void ThirdPartyFirewalls_Load(object sender, EventArgs e)
-		{
-			foreach (FirewallProduct registeredProduct in FirewallManager.RegisteredProducts)
-			{
-				listBoxFirewalls.Items.Add(registeredProduct.Name);
-			}
-		}
-	}
+        private void ThirdPartyFirewalls_Load(object sender, EventArgs e)
+        {
+            foreach (FirewallProduct registeredProduct in FirewallManager.RegisteredProducts)
+            {
+                listBoxFirewalls.Items.Add(registeredProduct.Name);
+            }
+        }
+    }
 }
